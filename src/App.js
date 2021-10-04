1190303585f86dbae041a9ae2cde48c786c8e9a5
Preetpal Basson <preet.diablo@gmail.com>
2021-10-04 17:56:55 +0100

    V0.05: Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
 
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>New Project Page</h1>
+      <table className="table">
+        <tr>
+          <ul>Test1</ul>
+        </tr>
+        <tr>
+          <ul>Test2</ul>
+        </tr>
+        <tr>
+          <ul>Test3</ul>
+        </tr>
+      </table>
     </div>
   );
 }
