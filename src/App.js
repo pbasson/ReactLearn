420448606c942f4c9c2b3c3a05014dd6dbd5028c
Preetpal Basson <preet.diablo@gmail.com>
2021-10-05 11:56:08 +0100

    V0.05: Update

--- src/App.js
+++ src/App.js
@@ -9,21 +9,53 @@ import "./styles.css";
 //   );
 // }
 
+// const customStyle = {
+//   color: "red",
+//   fontSize: "20px"
+// };
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1 style={customStyle}>New Project Page</h1>
+//       <table className="table">
+//         <tr>
+//           <ul>Test1</ul>
+//         </tr>
+//         <tr>
+//           <ul>Test2</ul>
+//         </tr>
+//         <tr>
+//           <ul>Test3</ul>
+//         </tr>
+//       </table>
+//     </div>
+//   );
+// }
+
+const dateGet = new Date().getHours();
+const date = new Date(2021, 9, 1, 19).getHours();
+var place = "";
+const changer = {
+  color: ""
+};
+
+if (date < 12) {
+  place = "Good Morning";
+  changer.color = "green";
+} else if (date < 18) {
+  place = "Good Afternoon";
+  changer.color = "yellow";
+} else {
+  place = "Good Evening";
+  changer.color = "Red";
+}
 export default function App() {
   return (
     <div className="App">
-      <h1>New Project Page</h1>
-      <table className="table">
-        <tr>
-          <ul>Test1</ul>
-        </tr>
-        <tr>
-          <ul>Test2</ul>
-        </tr>
-        <tr>
-          <ul>Test3</ul>
-        </tr>
-      </table>
+      <h1 className="headings" style={changer}>
+        {place}
+      </h1>
     </div>
   );
 }
